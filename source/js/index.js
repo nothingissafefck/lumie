c99e962a669c24161a7625a92f391d938191053c
Victoria Yakovleva <vikyss@mail.ru>
2023-11-01 18:33:19 +0300

    feat: add logo

--- source/js/index.js
+++ source/js/index.js
@@ -2,10 +2,12 @@ import { initTabs } from './tabs'
 import {initExpandable} from './expandable';
 import {initDialog} from "./dialog";
 import {initSwiper} from "./swiper";
+import {initMenu} from './menu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initTabs()
     initExpandable();
     initDialog()
     initSwiper()
+    initMenu()
 })
---
+++ source/js/menu.js
@@ -0,0 +1,37 @@
+const menu = document.querySelector('[data-menu]');
+const toggleMenuControls = document.querySelectorAll('[data-menu-toggle]');
+const closeMenuControls = document.querySelectorAll('[data-menu-close]');
+const content = document.querySelector('main');
+
+function toggleMenu(close) {
+    const force = close !== undefined ? !close : undefined;
+
+    menu.classList.toggle('is-open', force);
+    content.classList.toggle('menu-open', force);
+}
+
+function onBackdropClick(event) {
+    if (event.target.closest('[data-menu]')) {
+        return;
+    }
+
+    document.body.removeEventListener('click', onBackdropClick, true);
+    toggleMenu(true);
+}
+
+function onMenuToggle(close) {
+    if (menu.classList.contains('is-open')) {
+        document.body.removeEventListener('click', onBackdropClick, true);
+        toggleMenu(close);
+
+        return;
+    }
+
+    document.body.addEventListener('click', onBackdropClick, true);
+    toggleMenu(close);
+}
+
+export function initMenu() {
+    toggleMenuControls.forEach((control) => control.addEventListener('click', () => onMenuToggle()));
+    closeMenuControls.forEach((control) => control.addEventListener('click', () => onMenuToggle(true)));
+}
